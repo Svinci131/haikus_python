a4972e6adff6e180fe32b3ce80614aeded148336
Svinci131 <svinci131@gmail.com>
2016-08-01 19:46:04 -0400

    get syllables

--- index.py
+++ index.py
@@ -1,20 +1,53 @@
 import os
 from syllableCounting import getSyllables 
 
+def testSyllables():
+	tests = {
+		'Ferret': 2, 
+		'about': 2, 
+		'shoe': 1, 
+		'Boy': 1, 
+		'lynx':1,
+		'happy':2,
+		'preserve': 2,
+		'perseverance': 4,
+		'resevior': 3, 
+		"engagement": 3,
+		"data": 2,
+	}
+
+	for key, value in tests.items():
+		sylables = getSyllables(key)
+		if (sylables != value):
+			print (key, sylables, value)
+
 # (list of words) => haiku as str
 def createHaiku(data):
 	haiku = "";
 	order = [5,7,5]; 
-	for word in data:
-		getSyllables(word)
+	testSyllables()
+	# print (getSyllables('Ferret'), "should be 2")
+	# print (getSyllables('about'), "should be 2"); 
+	# print (getSyllables('shoe'), "should be 1"); 
+	# print (getSyllables('Boy'), "should be 1"); 
+	# print (getSyllables('lynx'),"should be 1");
+	# print (getSyllables('happy'),"should be 2");
+	# print (getSyllables('preserve'), "sould be two");
+	# print (getSyllables('resevior'), "should be 3"); 
+	# print (getSyllables("engagement"), "should be 3")
+	# for word in data:
+	# 	getSyllables(word)
 
 # (str) => list of words
-# def readFile (fileName):
-dirpath = os.path.relpath('textFiles')
-fpath = os.path.join(dirpath, "timeMachine.txt")
-f = open(fpath)
-content = f.read().split();
-createHaiku(content)
+def readFile (fileName):
+	dirpath = os.path.relpath('textFiles')
+	fpath = os.path.join(dirpath, fileName)
+	f = open(fpath)
+	content = f.read().split();
+	return content
+
+data = readFile("timeMachine.txt")
+createHaiku(data)
 
 
 
--- syllableCounting.py
+++ syllableCounting.py
@@ -1,7 +1,34 @@
-# def isVowel(string):
-	
+# (str) => boolean - O(1)
+def isVowel(letter, i):
+	if letter == "y" and (i == 0):
+		return True 
+	if letter in ('a', 'e', 'i', 'o', 'u', 'y'):
+		return True;
+	else: 
+		return False
 # (str) => num
 def getSyllables(string):
-	print string
+
+	syllables = 0
+	letters = list(string.lower());
+	last = len(letters)-1;
+
+	for index, letter in enumerate(letters):
+		prev = letters[(index-1)]; 
+
+		# if a cosnant is proceeded by a val add a syllable
+		if (isVowel(letter, index) == False and isVowel(prev, index) == True): 
+			syllables+=1
+
+		# if the last letter is a "e"
+		if (index == last and letter == 'e' and isVowel(prev, index) == False): 
+			syllables-=1
+		
+	return syllables
+
+# ('preserve', 3, 2)
+# ('engagement', 4, 3)
+# ('lynx', 0, 1)
+# ('happy', 1, 2)
 	#get all vowels that aren't the last letter and aren't next to another vowel
 	#your, he, quiet, boy, lynx, happy, preserve, resevior 
